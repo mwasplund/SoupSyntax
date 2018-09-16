cc43c5b664089ef69748b448bcd0fe491030b815
Matthew Asplund <mwasplund@outlook.com>
2018-09-16 12:07:20 -0700

    Move folder

--- generate.cmd
+++ generate.cmd
@@ -1,2 +1,2 @@
-antlr4 -Dlanguage=Cpp Grammar\CppLexer.g4 -o Syntax\Src\
-antlr4 -Dlanguage=Cpp Grammar\CppParser.g4 -o Syntax\Src\
+antlr4 -Dlanguage=Cpp Grammar\CppLexer.g4 -o Syntax\
+antlr4 -Dlanguage=Cpp Grammar\CppParser.g4 -o Syntax\
