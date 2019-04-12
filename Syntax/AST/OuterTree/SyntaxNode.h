488955684ef43572c6df04430c385c517241cc33
Matthew Asplund <mwasplund@outlook.com>
2019-04-11 20:16:34 -0700

    Move into outer tree folder

--- Syntax/AST/OuterTree/SyntaxNode.h
+++ Syntax/AST/OuterTree/SyntaxNode.h
@@ -8,12 +8,13 @@ namespace Soup::Syntax
     /// </summary>
     export class SyntaxNode
     {
-    public:
+    protected:
         /// <summary>
         /// Initialize
         /// </summary>
         SyntaxNode(SyntaxNodeType type) :
             m_type(type),
+            m_parent(nullptr)
             m_span(),
             m_fullSpan()
         {
@@ -22,9 +23,40 @@ namespace Soup::Syntax
         /// <summary>
         /// Get the node type
         /// </summary>
+        void SetParent(SyntaxNode* value) const
+        {
+            if (value == nullptr)
+                throw std::runtime_error("Cannot set parent to null.");
+            if (m_parent != nullptr)
+                throw std::runtime_error("Cannot set parent. Already set.");
+            m_parent = value;
+        }
+
+    public:
+        /// <summary>
+        /// Get the node type
+        /// </summary>
         SyntaxNodeType GetType() const
         {
             return m_type;
+        }
+
+        /// <summary>
+        /// Get a value indicating whether the node has a parent attached
+        /// </summary>
+        bool HasParent() const
+        {
+            return m_parent != nullptr;
+        }
+
+        /// <summary>
+        /// Get the parent node
+        /// </summary>
+        SyntaxNode& GetParent() const
+        {
+            if (m_parent == nullptr)
+                throw std::runtime_error("No parent attached.");
+            return *m_parent;
         }
 
         /// <summary>
@@ -78,5 +110,6 @@ namespace Soup::Syntax
         SyntaxNodeType m_type;
         TextSpan m_span;
         TextSpan m_fullSpan;
+        SyntaxNode* m_parent;
     };
 }
