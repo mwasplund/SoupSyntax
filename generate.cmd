414f089ee28485e71dcb368e71745d91c698180e
Matthew Asplund <mwasplund@outlook.com>
2019-07-27 08:58:48 -0700

    Move parser into own folder to allow for pulling in only the syntax tree if I only want to gen code

--- generate.cmd
+++ generate.cmd
@@ -1,2 +1,2 @@
-call antlr4 -Dlanguage=Cpp Grammar\CppLexer.g4 -o Syntax\Parser\
-call antlr4 -Dlanguage=Cpp Grammar\CppParser.g4 -visitor -lib Syntax\Parser\Grammar\ -o Syntax\Parser\
+call antlr4 -Dlanguage=Cpp Grammar\CppLexer.g4 -o Source\Parser\
+call antlr4 -Dlanguage=Cpp Grammar\CppParser.g4 -visitor -lib Source\Parser\Grammar\ -o Source\Parser\
